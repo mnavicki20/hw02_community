0c47b569e322ffca4980ef71029db45d46134e47
Mikita Navicki <bulbanik91@gmail.com>
2021-11-05 18:09:56 +0300

    Создано приложение Users, создана форма регистрации

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -20,6 +20,9 @@ class Post(models.Model):
         null=True,
     )
 
+    def __str__(self):
+        return self.text
+
     class Meta:
         ordering = ['-pub_date']
 
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
