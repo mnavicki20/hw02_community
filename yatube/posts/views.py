aa87efba9fcb3c89069fe7e2d45d5e8747f6ce32
Mikita Navicki <bulbanik91@gmail.com>
2021-11-06 19:07:12 +0300

    Добавлен декоратор @login_required, скорректирован файл forms.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import login
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 
 from .models import Group, Post
 
 
+@login_required
 def index(request):
     posts = Post.objects.all()[:10]
     template = 'posts/index.html'
